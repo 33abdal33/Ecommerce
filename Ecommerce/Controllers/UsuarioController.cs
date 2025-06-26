d7e5ddc601703347fe3e77deee4b9c6671492d8a
33abdal33 <abdallizanomuriel08@gmail.com>
2025-06-26 03:06:15 -0500

    Agregar archivos de proyecto.

---
+++ Ecommerce/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
