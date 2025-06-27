5fa5a8d298a161b4dd9bfe8a4e670dfd90e2940b
33abdal33 <abdallizanomuriel08@gmail.com>
2025-06-27 03:57:23 -0500

    Add services DBContext

--- Ecommerce/Program.cs
+++ Ecommerce/Program.cs
@@ -1,5 +1,13 @@
+using Ecommerce.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<ApplicationDbContext>(options=>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+    
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
