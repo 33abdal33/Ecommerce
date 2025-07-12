8c4aa668e6b069885040f57335ba39228b5ce4a4
33abdal33 <abdallizanomuriel08@gmail.com>
2025-07-12 01:15:19 -0500

    Add ApplicationDbContext

--- Ecommerce/Persistence/ApplicationDbContext.cs
+++ Ecommerce/Persistence/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace Ecommerce.Persistence
@@ -7,8 +8,14 @@ namespace Ecommerce.Persistence
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-
+            
         }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<DetallePedido> DetallesPedidos { get; set; }
+        public DbSet<CarritoItem> CarritoItems { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
