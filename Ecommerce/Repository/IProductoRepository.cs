6fa31de48ca384213289f552fcf452037e315135
33abdal33 <abdallizanomuriel08@gmail.com>
2025-07-12 01:15:35 -0500

    Add Repository

---
+++ Ecommerce/Repository/IProductoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Models;
+
+namespace Ecommerce.Repository
+{
+    public interface IProductoRepository
+    {
+        Task<List<Producto>> ObtenerTodosAsync();
+        Task<Producto> ObtenerPorIdAsync(int id);
+        Task CrearAsync(Producto producto);
+        Task ActualizarAsync(Producto producto);
+        Task EliminarAsync(Producto producto);
+        Task GuardarCambiosAsync();
+    }
+}
---
+++ Ecommerce/Repository/ProductoRepository.cs
@@ -0,0 +1,44 @@
+﻿using Ecommerce.Models;
+using Ecommerce.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Repository
+{
+    public class ProductoRepository : IProductoRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductoRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Producto>> ObtenerTodosAsync() =>
+             await _context.Productos.Include(p => p.Categoria).ToListAsync();
+
+        public async Task<Producto?> ObtenerPorIdAsync(int id) =>
+             await _context.Productos.Include(p => p.Categoria).FirstOrDefaultAsync(p => p.Id == id);
+
+        public async Task CrearAsync(Producto producto)
+        {
+            await _context.Productos.AddAsync(producto);
+        }
+
+        public Task ActualizarAsync(Producto producto)
+        {
+            _context.Productos.Update(producto);
+            return Task.CompletedTask;
+        }
+
+        public Task EliminarAsync(Producto producto)
+        {
+            _context.Productos.Remove(producto);
+            return Task.CompletedTask;
+        }
+
+        public async Task GuardarCambiosAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
