9db267de3dc64cdc807f3550d5a45448fa23701a
33abdal33 <abdallizanomuriel08@gmail.com>
2025-07-12 01:14:38 -0500

    Update Producto Controller

---
+++ Ecommerce/Controllers/ProductoController.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly IProductoService _productoService;
+        public ProductoController(IProductoService productoService)
+        {
+            _productoService = productoService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var productos = await _productoService.ObtenerCatalogoAsync();
+            return View(productos);
+        }
+        public IActionResult Crear()
+        {
+            return View();
+        }
+    }
+}
