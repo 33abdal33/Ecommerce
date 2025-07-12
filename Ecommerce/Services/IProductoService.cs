f10c9f82006952e432e4d50db19b5c5434e59ec6
33abdal33 <abdallizanomuriel08@gmail.com>
2025-07-12 01:15:47 -0500

    Add Service

---
+++ Ecommerce/Services/IProductoService.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.Models;
+
+namespace Ecommerce.Services
+{
+    public interface IProductoService
+    {
+        Task<List<Producto>> ObtenerCatalogoAsync();
+        Task<Producto?> ObtenerPorIdAsync(int id);
+        Task CrearProductoAsync(Producto producto);
+        Task ActualizarProductoAsync(Producto producto);
+        Task EliminarProductoAsync(int id);
+    }
+}
---
+++ Ecommerce/Services/ProductoService.cs
@@ -0,0 +1,43 @@
+﻿using Ecommerce.Models;
+using Ecommerce.Persistence;
+using Ecommerce.Repository;
+
+namespace Ecommerce.Services
+{
+    public class ProductoService : IProductoService
+    {
+        private readonly IProductoRepository _repo;
+
+        public ProductoService(IProductoRepository repo)
+        {
+            _repo = repo;
+        }
+        public Task<List<Producto>> ObtenerCatalogoAsync()
+        {
+            return _repo.ObtenerTodosAsync();
+        }
+        public Task<Producto?> ObtenerPorIdAsync(int id)
+        {
+            return _repo.ObtenerPorIdAsync(id);
+        }
+        public async Task CrearProductoAsync(Producto producto)
+        {
+            await _repo.CrearAsync(producto);
+            await _repo.GuardarCambiosAsync();
+        }
+        public async Task ActualizarProductoAsync(Producto producto)
+        {
+            await _repo.ActualizarAsync(producto);
+            await _repo.GuardarCambiosAsync();
+        }
+        public async Task EliminarProductoAsync(int id)
+        {
+            var producto = await _repo.ObtenerPorIdAsync(id);
+            if(producto is not null)
+            {
+                await _repo.EliminarAsync(producto);
+                await _repo.GuardarCambiosAsync();
+            }
+        }
+    }
+}
